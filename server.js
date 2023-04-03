4cc750c30ca7ccd820fe8dc19045a4f7a53ca159
abutler911 <abutler911@gmail.com>
2023-04-03 11:17:04 -0600

    adding routes

--- server.js
+++ server.js
@@ -15,14 +15,53 @@ app.set("views", __dirname + "/views");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Home page
 app.get("/", (req, res) => {
-  res.send("From the slash page");
+  res.render("index");
 });
 
+// Add Question page
 app.get("/questions/create", (req, res) => {
   res.render("questions/create");
 });
 
+// Start Quiz page
+app.get("/quiz", (req, res) => {
+  res.render("quiz");
+});
+
+// Modify Question page
+app.get("/questions/:id/edit", (req, res) => {
+  const id = req.params.id;
+
+  Question.findById(id)
+    .then((question) => {
+      res.render("questions/edit", { question });
+    })
+    .catch((err) => console.log(err));
+});
+
+// Submit modified question
+app.post("/questions/:id/edit", (req, res) => {
+  const id = req.params.id;
+  const { question, category, answer } = req.body;
+
+  Question.findByIdAndUpdate(
+    id,
+    {
+      question,
+      category,
+      answer,
+    },
+    { new: true }
+  )
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch((err) => console.log(err));
+});
+
+// Create new question
 app.post("/questions/create", (req, res) => {
   const { question, category, answer } = req.body;
 
@@ -35,7 +74,7 @@ app.post("/questions/create", (req, res) => {
   newQuestion
     .save()
     .then(() => {
-      res.redirect("/questions/create");
+      res.redirect("/");
     })
     .catch((err) => console.log(err));
 });
