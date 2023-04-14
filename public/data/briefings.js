e392c7cf92b03b7e9495f767c65bd18f865264e2
Andrew Butler <abutler911@gmail.com>
2023-04-13 19:21:52 -0600

    styling and added time to the notes

--- public/data/briefings.js
+++ public/data/briefings.js
@@ -12,12 +12,14 @@ export const cardContents = [
   {
     id: "want-briefing",
     title: "WANT Briefing",
-    content: "FILL IN CONTENT HERE",
+    content:
+      "W-weather, discuss current weather and any issues. A - airport, start at the gate, talk about the pushback, taxi-out, hot spots, runway crossings",
   },
   {
     id: "welcome-aboard-announcement",
     title: "Welcome Aboard Announcement",
-    content: "FILL IN CONTENT HERE",
+    content:
+      "Ladies and gentlemen, welcome aboard _________ flight ______ service to _________. We're going to be pushing back here in just a few minutes and it's a short/long taxi out to the runway. Once we're in the air, flight time today is ______. We are expecting / not expecting a few bumps on the way out today. In the back you have _____ and _____. They are there primarily for your safety so I would appreciate you giving them your attention during their safety announcements. We'll be pushing back shortly, we do appreciate your business and welcome aboard flight ______.",
   },
   {
     id: "toc-briefing",
