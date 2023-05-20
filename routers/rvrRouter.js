1ed976355df51d2c0b4cb572ccbfaa6121b4bea5
abutler911 <abutler911@gmail.com>
2023-05-20 09:51:21 -0600

    fixed rvr page

--- routers/rvrRouter.js
+++ routers/rvrRouter.js
@@ -1,23 +1,12 @@
 const express = require("express");
 const router = express.Router();
-// function generateRVRData() {
-//   const segments = ["segment1", "segment2", "segment3"];
-//   let rvrData = {};
+const { isLoggedIn } = require("../middleware/middlewares");
 
-//   segments.forEach((segment) => {
-//     const range = Math.floor(Math.random() * 100);
-//     rvrData[segment] = {
-//       range: range,
-//       solution: getSolutionForRange(range),
-//     };
-//   });
-
-//   return rvrData;
-// }
-
-router.get("/rvr", (req, res) => {
-  //   const data = generateRVRData();
-  res.render("rvr", { title: "RVR Simulator", customCSS: "rvr.css" });
+router.get("/rvr", isLoggedIn, (req, res) => {
+  res.render("rvr", {
+    title: "RVR Simulator",
+    customCSS: "rvr.css",
+  });
 });
 
 module.exports = router;
