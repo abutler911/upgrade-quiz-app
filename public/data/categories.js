fa0b51c74421b644fe205707798ba5918b6d9e25
Andrew Butler <abutler911@gmail.com>
2023-05-25 07:38:55 -0600

    edit questions page changes

--- public/data/categories.js
+++ public/data/categories.js
@@ -1,219 +1,210 @@
 const categories = [
   {
-    "id": 1,
-    "name": "ACARS",
-    "value": "acars"
+    id: 1,
+    name: "ACARS",
+    value: "acars",
   },
   {
-    "id": 2,
-    "name": "Administration",
-    "value": "administration"
+    id: 2,
+    name: "Administration",
+    value: "administration",
   },
   {
-    "id": 3,
-    "name": "Adverse Weather",
-    "value": "adverse-weather"
+    id: 3,
+    name: "Adverse Weather",
+    value: "adverse-weather",
   },
   {
-    "id": 4,
-    "name": "Aerodynamics",
-    "value": "aerodynamics"
+    id: 4,
+    name: "Aerodynamics",
+    value: "aerodynamics",
   },
   {
-    "id": 5,
-    "name": "AFCS",
-    "value": "AFCS"
+    id: 5,
+    name: "AFCS",
+    value: "AFCS",
   },
   {
-    "id": 6,
-    "name": "Air Management System",
-    "value": "air-management-system"
+    id: 6,
+    name: "Air Management System",
+    value: "air-management-system",
   },
   {
-    "id": 7,
-    "name": "Aircraft Security Procedures",
-    "value": "aircraft-security-procedures"
+    id: 7,
+    name: "Aircraft Security Procedures",
+    value: "aircraft-security-procedures",
   },
   {
-    "id": 8,
-    "name": "Airworthiness",
-    "value": "airworthiness"
+    id: 8,
+    name: "Airworthiness",
+    value: "airworthiness",
   },
   {
-    "id": 9,
-    "name": "APU",
-    "value": "APU"
+    id: 9,
+    name: "APU",
+    value: "APU",
   },
   {
-    "id": 10,
-    "name": "Communications Equipment",
-    "value": "communications-equipment"
+    id: 10,
+    name: "Communications Equipment",
+    value: "communications-equipment",
   },
   {
-    "id": 11,
-    "name": "Deice",
-    "value": "deice"
+    id: 11,
+    name: "Deice",
+    value: "deice",
   },
   {
-    "id": 12,
-    "name": "Dispatch Release Explanation",
-    "value": "dispatch-release"
+    id: 12,
+    name: "Dispatch Release Explanation",
+    value: "dispatch-release",
   },
   {
-    "id": 13,
-    "name": "Electrical Systems",
-    "value": "electrical-systems"
+    id: 13,
+    name: "Electrical Systems",
+    value: "electrical-systems",
   },
   {
-    "id": 14,
-    "name": "Emergency Equipment",
-    "value": "emergency-equipment"
+    id: 14,
+    name: "Emergency Equipment",
+    value: "emergency-equipment",
   },
   {
-    "id": 15,
-    "name": "Emergency/Irreg Ops",
-    "value": "emergency/irreg-ops"
+    id: 15,
+    name: "Emergency/Irreg Ops",
+    value: "emergency/irreg-ops",
   },
   {
-    "id": 16,
-    "name": "Engines",
-    "value": "engines"
+    id: 16,
+    name: "Engines",
+    value: "engines",
   },
   {
-    "id": 17,
-    "name": "Fire Protection",
-    "value": "fire-protection"
+    id: 17,
+    name: "Fire Protection",
+    value: "fire-protection",
   },
   {
-    "id": 18,
-    "name": "Flight Controls & Stall Prot.",
-    "value": "flight-controls-and-stall-protection"
+    id: 18,
+    name: "Flight Controls & Stall Prot.",
+    value: "flight-controls-and-stall-protection",
   },
   {
-    "id": 19,
-    "name": "Flight Crew Requirements",
-    "value": "flight-crew-requirements"
+    id: 19,
+    name: "Flight Crew Requirements",
+    value: "flight-crew-requirements",
   },
   {
-    "id": 20,
-    "name": "Flight Instruments",
-    "value": "flight-instruments"
+    id: 20,
+    name: "Flight Instruments",
+    value: "flight-instruments",
   },
   {
-    "id": 21,
-    "name": "Flight Operations",
-    "value": "flight-operations"
+    id: 21,
+    name: "Flight Operations",
+    value: "flight-operations",
   },
   {
-    "id": 22,
-    "name": "FOM Chap. 3",
-    "value": "fom-chap-3"
+    id: 22,
+    name: "FOM Chap. 3",
+    value: "fom-chap-3",
   },
   {
-    "id": 23,
-    "name": "FOM Chap. 5",
-    "value": "fom-chap-5"
+    id: 23,
+    name: "FOM Chap. 5",
+    value: "fom-chap-5",
   },
   {
-    "id": 24,
-    "name": "Fuel Planning",
-    "value": "fuel-planning"
+    id: 24,
+    name: "Fuel Planning",
+    value: "fuel-planning",
   },
   {
-    "id": 25,
-    "name": "Fuel Systems",
-    "value": "fuel-systems"
+    id: 25,
+    name: "Fuel Systems",
+    value: "fuel-systems",
   },
   {
-    "id": 26,
-    "name": "Hazardous Materials",
-    "value": "hazardous-materials"
+    id: 26,
+    name: "Hazardous Materials",
+    value: "hazardous-materials",
   },
   {
-    "id": 27,
-    "name": "Human Factors",
-    "value": "human-factors"
+    id: 27,
+    name: "Human Factors",
+    value: "human-factors",
   },
   {
-    "id": 28,
-    "name": "Hydraulic System",
-    "value": "hydraulic-system"
+    id: 28,
+    name: "Hydraulic System",
+    value: "hydraulic-system",
   },
   {
-    "id": 29,
-    "name": "Ice & Rain Protection",
-    "value": "ice-rain-protection"
+    id: 29,
+    name: "Ice & Rain Protection",
+    value: "ice-rain-protection",
   },
   {
-    "id": 30,
-    "name": "Indoc",
-    "value": "indoc"
+    id: 30,
+    name: "Indoc",
+    value: "indoc",
   },
   {
-    "id": 31,
-    "name": "International Ops",
-    "value": "international-ops"
+    id: 31,
+    name: "International Ops",
+    value: "international-ops",
   },
   {
-    "id": 32,
-    "name": "Landing Gear and Brakes",
-    "value": "landing-gear-and-brakes"
+    id: 32,
+    name: "Landing Gear and Brakes",
+    value: "landing-gear-and-brakes",
   },
   {
-    "id": 33,
-    "name": "Limitations",
-    "value": "limitations"
+    id: 33,
+    name: "Limitations",
+    value: "limitations",
   },
   {
-    "id": 34,
-    "name": "Memory Items",
-    "value": "memory-items"
+    id: 34,
+    name: "Memory Items",
+    value: "memory-items",
   },
   {
-    "id": 35,
-    "name": "Navigation Systems",
-    "value": "navigation-systems"
+    id: 35,
+    name: "Navigation Systems",
+    value: "navigation-systems",
   },
   {
-    "id": 36,
-    "name": "Non-Routine Ops",
-    "value": "non-routine-ops"
+    id: 36,
+    name: "Non-Routine Ops",
+    value: "non-routine-ops",
   },
   {
-    "id": 37,
-    "name": "Org & Chain-of-Command",
-    "value": "org-chain-of-command"
+    id: 37,
+    name: "Org & Chain-of-Command",
+    value: "org-chain-of-command",
   },
   {
-    "id": 38,
-    "name": "Pressurization",
-    "value": "pressurization"
+    id: 38,
+    name: "Pressurization",
+    value: "pressurization",
   },
   {
-    "id": 39,
-    "name": "Supplemental Ops",
-    "value": "supplemental-ops"
+    id: 39,
+    name: "Supplemental Ops",
+    value: "supplemental-ops",
+  },
+
+  {
+    id: 42,
+    name: "Upset Recovery",
+    value: "upset-recovery",
   },
   {
-    "id": 40,
-    "name": "TEST CATEGORY",
-    "value": "test-category"
+    id: 43,
+    name: "Warning Systems",
+    value: "warning-systems",
   },
-  {
-    "id": 41,
-    "name": "TESTING CATEGORY",
-    "value": "testing"
-  },
-  {
-    "id": 42,
-    "name": "Upset Recovery",
-    "value": "upset-recovery"
-  },
-  {
-    "id": 43,
-    "name": "Warning Systems",
-    "value": "warning-systems"
-  }
 ];
 
 module.exports = categories;
